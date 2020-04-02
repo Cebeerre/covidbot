851dd965c5bbeb09e4ebeedd63889eb2c75b1bcb
Cebeerre <cebeerre@hotmail.com>
2020-04-02 16:20:18 +0200

    typo

--- restart_bot.sh
+++ restart_bot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PIDS=`ps -fea | grep covid_bot.py | grep -v grep | awk '{ print $2" "$3 }'`
 RUNCMD=`crontab -l | grep covid_bot.sh | sed 's/@reboot //g'`
-if [ "$PIDS"] ; then
+if [ "$PIDS" ] ; then
   echo "Killing "$PIDS
   kill -9 $PIDS
   $RUNCMD
